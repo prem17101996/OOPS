0cf69c24e913107d12e8b6fd02c609a84bd98484
unknown <premmishra17101996@gmail.com>
2020-02-12 21:03:01 +0530

    added a program by using inheritance

---
+++ inheritance.py
@@ -0,0 +1,12 @@
+class animal:
+    def eating(self):
+        print("eating")
+
+class dog():
+    def eating(self):
+        print("eating")
+    def bark(self):
+        print("bark")
+obj=dog()
+obj.eating()
+obj.bark()
